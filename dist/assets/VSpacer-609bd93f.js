b4825568cd23727464eb2bb40186a7f405b94c16
jeff su <jeff@octavainc.com>
2024-04-10 11:37:50 -0400

    release as version 1.0

---
+++ dist/assets/VSpacer-609bd93f.js
@@ -0,0 +1,1 @@
+import"./VContainer-0b5af25b.js";import{c as r}from"./createSimpleFunctional-cb14a061.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- dist/assets/VSpacer-6fae0fcb.js
+++
@@ -1,1 +0,0 @@
-import"./VContainer-25d587d6.js";import{c as r}from"./createSimpleFunctional-9b97d869.js";const e=r("v-spacer","div","VSpacer");export{e as V};
